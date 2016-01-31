94067c891ec22f980bafc4d5cd4ef17cf0a791c0
Jacob Dearing <jacob.dearing@gmail.com>
2016-01-31 07:09:58 -0600

    Delete after_install.sh

--- scripts/after_install.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-cd $HOME
-echo "export GOPATH=$HOME/golang" >> $HOME/.bashrc
-echo "export PATH=$PATH:$GOPATH/bin" >> $HOME/.bashrc
-source $HOME/.bashrc
-cd $HOME/golang/src/github.com/dearing/havoc_server
-go build
-go install
